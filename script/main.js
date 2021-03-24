e212033afacbe0f8a2cf410bdd0c85bdbf3127d7
ryazancev.e <ryazancev.e@gmail.com>
2021-03-24 19:07:26 +0300

    без футера и адаптива

--- script/main.js
+++ script/main.js
@@ -58,4 +58,52 @@ navSite.addEventListener('click', evt => {
     };
 });
 
+//Видео
+
+function findVideos() {
+    var videos = document.querySelectorAll('.video');
+
+    for (var i = 0; i < videos.length; i++) {
+        setupVideo(videos[i]);
+    }
+}
+
+function setupVideo(video) {
+    var link = video.querySelector('.video__link');
+    var media = video.querySelector('.video__media');
+    var button = video.querySelector('.video__button');
+    var id = parseMediaURL(media);
+    video.addEventListener('click', function () {
+        var iframe = createIframe(id);
+        link.remove();
+        button.remove();
+        video.appendChild(iframe);
+    });
+    link.removeAttribute('href');
+    video.classList.add('video--enabled');
+}
+
+function parseMediaURL(media) {
+    var regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+    var url = media.src;
+    var match = url.match(regexp);
+    return match[1];
+}
+
+function createIframe(id) {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('allow', 'autoplay');
+    iframe.setAttribute('src', generateURL(id));
+    iframe.classList.add('video__media');
+    return iframe;
+}
+
+function generateURL(id) {
+    var query = '?rel=0&showinfo=0&autoplay=1';
+    return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideos();
+
 
